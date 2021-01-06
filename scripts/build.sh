1e229cee3767ad5ede92bb9be4579c932bbb7b94
Faraz Shaikh <farazzshaikh@gmail.com>
2021-01-06 16:55:24 +0530

    Fixed Workflows

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 # Compile Library
 rollup -c &&
 # Compile CLI
-cd cli && npm run build && cd .. &&
+cd cli && npm install && npm run build && cd .. &&
 # Copy over logo
 cp ./cli/bin/logo.txt ./cli/dist/ &&
 # Delete Compiled files
