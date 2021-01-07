864459b23fe1e74f18e27958bf9d22d2cc4f9d02
Faraz Shaikh <farazzshaikh@gmail.com>
2021-01-07 23:45:01 +0530

    Removed unused exports

---
+++ utils/msgPack/README.md
@@ -0,0 +1,5 @@
+
+<p align="center">
+  <h1 align="center"><a href="https://github.com/mprot/msgpack-js">msgpack by mprot</a></h1>
+   <h3 align="center">Under MIT License</h1>
+</p>
--- utils/msgPack/index.ts
+++ utils/msgPack/index.ts
@@ -1,34 +1,13 @@
-import {
-	Type, Collection, Obj,
-	Nil, Bool, Int, Uint, Float, Bytes, Str, TypedArr, TypedMap, Time, Any, Arr, Map, Struct, Union,
-	structEncoder, structDecoder, unionEncoder, unionDecoder,
-} from "./types";
-import {
-	WriteBuffer, ReadBuffer,
-	createWriteBuffer, createReadBuffer,
-} from "./buffer";
+import { Type, Any } from "./types";
+import { createWriteBuffer, createReadBuffer } from "./buffer";
 
-
-export {
-	WriteBuffer, ReadBuffer,
-	Type, Collection, Obj,
-	Nil, Bool, Int, Uint, Float, Bytes, Str, TypedArr, TypedMap, Time, Any, Arr, Map, Struct, Union,
-	structEncoder, structDecoder, unionEncoder, unionDecoder,
-	encode, decode,
-};
-
-
-
-
-
+export { encode, decode };
 
 function encode<T>(v: T, typ?: Type<T>): Uint8Array {
 	const buf = createWriteBuffer();
 	(typ || Any).enc(buf, v);
 	return buf.ui8array();
 }
-
-
 
 function decode<T>(buf: BufferSource, typ?: Type<T>): T {
 	return (typ || Any).dec(createReadBuffer(buf));
